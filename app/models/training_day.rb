e1f1c76aa4b79022265d8b9f03fee2abd18553ba
rirakkumakuma <rkcrkkt.417@gmail.com>
2022-06-13 13:32:51 +0000

    [Update]練習出欠確認機能

--- app/models/training_day.rb
+++ app/models/training_day.rb
@@ -2,4 +2,11 @@ class TrainingDay < ApplicationRecord
   belongs_to :orchestra
   has_many :trainings, dependent: :destroy
   has_many :members, through: :trainings
+
+  def training_date
+    training_day.strftime('%Y年%-m月%-d日 %H時%M分')
+    # @weeks = ["日","月","火","水","木","金","土"]
+    # @num = training_day.strftime("%w").to_i
+    # @week = @weeks[num]
+  end
 end
