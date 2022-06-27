95fb64219a41449058745c7a8cf48322b18f5033
rirakkumakuma <rkcrkkt.417@gmail.com>
2022-06-27 15:55:36 +0000

    [Update レイアウト調整]

--- app/controllers/orchestra_managers_controller.rb
+++ app/controllers/orchestra_managers_controller.rb
@@ -6,7 +6,7 @@ class OrchestraManagersController < ApplicationController
     @member.orchestra_managers.where(orchestra: @orchestra).first.update_attributes(is_active: active_status)
     if active_status == 0
       flash[:notice] = "復団処理を実行いたしました"
-    elsif active_status== 1
+    elsif active_status == 1
       flash[:notice] = "休団処理を実行いたしました"
     else
       flash[:notice] = "退団処理を実行いたしました"
