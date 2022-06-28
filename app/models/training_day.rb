d8a4b342eccc28ac8b8d749199fbb26f087951de
rirakkumakuma <rkcrkkt.417@gmail.com>
2022-06-28 16:30:05 +0000

    [Update]練習出欠確認レイアウト

--- app/models/training_day.rb
+++ app/models/training_day.rb
@@ -3,6 +3,8 @@ class TrainingDay < ApplicationRecord
   has_many :trainings, dependent: :destroy
   has_many :members, through: :trainings
 
+　default_scope -> { order(created_at: :desc) }
+
   def training_date
     training_day.strftime('%Y年%-m月%-d日 %H時%M分')
   end
