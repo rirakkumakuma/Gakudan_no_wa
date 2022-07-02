a1ba4e7dd62874a5786ecfdd4036cdb801ea4298
rirakkumakuma <rkcrkkt.417@gmail.com>
2022-07-02 04:52:47 +0000

    [Update Topページレイアウト]

--- app/controllers/training_days_controller.rb
+++ app/controllers/training_days_controller.rb
@@ -2,8 +2,7 @@ class TrainingDaysController < ApplicationController
   def index
     @orchestra = Orchestra.find(params[:orchestra_id])
     @training_day = TrainingDay.new(orchestra_id: params[:orchestra_id])
-    # orchestra = Orchestra.find(params[:orchestra_id])
-    # @training_days = orchestra.training_days.order(created_at: :desc)
+    @training_days = @orchestra.training_days.order(created_at: :desc)
   end
 
   def create
