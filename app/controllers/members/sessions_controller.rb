840dc54bdd177d116880013ce6d97f3a9d39053a
rirakkumakuma <rkcrkkt.417@gmail.com>
2022-07-18 13:30:24 +0000

    [UPdate]

--- app/controllers/members/sessions_controller.rb
+++ app/controllers/members/sessions_controller.rb
@@ -5,9 +5,9 @@ module Members
     # before_action :configure_sign_in_params, only: [:create]
 
     # GET /resource/sign_in
-    def new
-      super
-    end
+    # def new
+    #   super
+    # end
 
     # POST /resource/sign_in
     # def create
