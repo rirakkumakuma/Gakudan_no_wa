8b63c7b895db6f74d5c529d99623edcd26793650
rirakkumakuma <rkcrkkt.417@gmail.com>
2022-07-19 14:02:37 +0000

    [Update]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto: true,
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'smtp.gmail.com',
+    authentication: 'login',
+    user_name: 'keikoyamazaki0703@gmail.com',
+    password: 'ilfwxxrjjwvijfjo'
+  }
+
   host = 'samplehost'
-  Rails.application.routes.default_url_options[:host] = host
+  config.action_mailer.default_url_options = {  host: 'samplehost' }
 end
