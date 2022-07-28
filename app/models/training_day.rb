0ea247a57e2561a27398c1d8b8079f401d5be37a
rirakkumakuma <rkcrkkt.417@gmail.com>
2022-07-28 02:43:59 +0000

    [Update 練習出欠確認]

--- app/models/training_day.rb
+++ app/models/training_day.rb
@@ -5,6 +5,9 @@ class TrainingDay < ApplicationRecord
   has_many :trainings, dependent: :destroy
   has_many :members, through: :trainings
 
+  validates :training_day, :title, :body, presence: true
+
+
   def training_date
     training_day.strftime('%Y年%-m月%-d日 %H時%M分')
   end
