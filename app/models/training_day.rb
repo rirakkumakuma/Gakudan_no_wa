748d53811a64931ddc72170fd74599b943eb753b
rirakkumakuma <rkcrkkt.417@gmail.com>
2022-08-07 13:13:24 +0000

    [Update レイアウト調整]

--- app/models/training_day.rb
+++ app/models/training_day.rb
@@ -5,8 +5,11 @@ class TrainingDay < ApplicationRecord
   has_many :trainings, dependent: :destroy
   has_many :members, through: :trainings
 
-  validates :training_day, :title, :body, presence: true
-
+  validates :training_day, presence: true
+  validates :title,
+    length:{ minimum: 1, maximum: 100 },presence: true
+  validates :body,
+    length:{ minimum: 1, maximum: 1000 },presence: true
 
   def training_date
     training_day.strftime('%Y年%-m月%-d日 %H時%M分')
